5603b8129f98f69df28786a4fb1719a34f1cf7d9
balswyan <balswyan@github.com>
2019-02-18 18:30:18 -0500

    Added the Youtube Channel link and NEED to work on the Description Look up

--- Youtube/YouTube.py
+++ Youtube/YouTube.py
@@ -1,6 +1,9 @@
+import urllib2
+from cookielib import CookieJar
 from bs4 import BeautifulSoup
 import re
 import os
+import time
 import json
 
 def main():
@@ -10,6 +13,11 @@ def main():
     data = _get_json(soup)
     get_youtube_info(data)
 
+cookies = CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies))
+opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 '
+                                    '(KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17')]	
+	
 def get_youtube_info(json_data):
     #Title, Views
     videoPrimaryInfoRenderer = json_data[0]["videoPrimaryInfoRenderer"]
@@ -25,15 +33,16 @@ def get_youtube_info(json_data):
     dislikes_count = topLevelButtons[1]["toggleButtonRenderer"]["defaultText"]["accessibility"]["accessibilityData"]["label"]
     dislikes_count_clean = _strip_non_numeric(dislikes_count)
 
-    #Channel Name, Channel ID, Subscriber Count
+    #Channel Name, Channel ID, Channel Link, Subscriber Count
     videoSecondaryInfoRenderer = json_data[1]["videoSecondaryInfoRenderer"]
     videoOwnerRender = videoSecondaryInfoRenderer["owner"]["videoOwnerRenderer"]
     channel_name = videoOwnerRender["title"]["runs"][0]["text"]
     channel_id = videoOwnerRender["title"]["runs"][0]["navigationEndpoint"]["browseEndpoint"]["browseId"]
     subscriber_count = videoOwnerRender["subscriberCountText"]["simpleText"]
     subscriber_count_clean = _strip_non_numeric(subscriber_count)
-
-    #Description
+    channel_link = 'youtube.com/' + channel_name
+	
+	#Description
     description_runs = videoSecondaryInfoRenderer["description"]["runs"]
     description = ""     
     for text in description_runs:
@@ -43,7 +52,8 @@ def get_youtube_info(json_data):
     #Category
     category_runs = videoSecondaryInfoRenderer["metadataRowContainer"]["metadataRowContainerRenderer"]["rows"][0]["metadataRowRenderer"]["contents"][0]["runs"]
     categories = [cat["text"] for cat in category_runs]
-
+    
+	
     html_info = {
         'title':video_title,
         'views':view_count_clean,
@@ -51,16 +61,24 @@ def get_youtube_info(json_data):
         'dislikes':dislikes_count_clean,
         'channel_name:':channel_name,
         'channel_id:':channel_id,
+        'channel_link:':channel_link,				
         'subscriber_count':subscriber_count_clean,
         'description':description,
         'categories':categories
         }
     return html_info
-    
-    
 
 
 
+	#Channel Description #Need to refrence channel_name from get_youtube_info so that we get the description 
+def description_lookup():
+		channel_about_link = 'https://youtube.com/' + channel_name + '/about'	
+		source = opener.open(channel_about_link).read()
+		findLinks = re.findall(r'description" content="(.*?)"', source)
+		for eachThing in findLinks:
+			print(eachThing)
+
+description_lookup()
 
 def _strip_non_numeric(item):
     """
