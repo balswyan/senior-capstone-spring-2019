957d267720a8ba5ce4c66eb70aaf855d8a8ea11d
rsajjan3 <32497743+rsajjan3@users.noreply.github.com>
2019-02-23 16:06:06 -0500

    Finished YouTube parser

--- Scripts/Youtube/YouTube.py
+++ Scripts/Youtube/YouTube.py
@@ -4,15 +4,19 @@ import os
 import time
 import json
 from bs4 import BeautifulSoup
+from HTMLParser import HTMLParser
+import xml.etree.ElementTree as ET
+
 
 def main():
-    with open('Page.html', 'r') as f:
+    with open('Scripts/Youtube/Page2.html', 'r') as f:
         html = f.read()
     soup = BeautifulSoup(html, 'html.parser')
-    data = _get_json(soup)
-    get_youtube_info(data)
+    get_youtube_info(soup)
 	
-def get_youtube_info(json_data):
+def get_youtube_info(html_soup):
+    json_data = _get_json(html_soup)
+
     #Title, Views
     videoPrimaryInfoRenderer = json_data[0]["videoPrimaryInfoRenderer"]
     video_title = videoPrimaryInfoRenderer["title"]["simpleText"]
@@ -49,6 +53,9 @@ def get_youtube_info(json_data):
 	#Channel description
     channel_desc = description_lookup(channel_name)
 
+    #Captions
+    caption = caption_parser(html_soup)
+
     html_info = {
         'title':video_title,
         'views':view_count_clean,
@@ -59,9 +66,33 @@ def get_youtube_info(json_data):
         'subscriber_count':subscriber_count_clean,
         'description':description,
         'channel_description':channel_desc,
+        'caption':caption,
         'categories':categories
         }
     return html_info
+
+def caption_parser(html_soup):
+    caption_url = ''
+    caption_re = re.search(r'timedtext[?](.*?)"', html_soup.text)
+    if caption_re is not None:
+        caption_url = caption_re.group(1).replace("\\\\u0026", "&").replace("%2C",",").replace("\\", "")
+    else: #No captions found
+        return None
+    
+    caption_url = "https://youtube.com/api/timedtext?" + caption_url
+    xml = _get_request(caption_url)
+
+    caption_text = ''
+    hparser = HTMLParser()
+    xml = BeautifulSoup(xml, features='lxml')
+
+    xml = xml.findAll(text=True)
+    for piece in xml:
+        caption_text += re.sub(r'<font color=\"(.*?)\">', '', piece).replace('</font>', '') + " "
+    caption_text = caption_text.replace('xml version="1.0" encoding="utf-8" ?', '')
+    caption_text = hparser.unescape(caption_text)
+    
+    return caption_text
 
 def description_lookup(channel_name):
     """
@@ -71,12 +102,23 @@ def description_lookup(channel_name):
     
     :return channel_desc (string): Channel description
     """
+    html = _get_request('https://youtube.com/' + channel_name + '/about')
+    channel_desc = re.search(r'description" content="(.*?)"', html)
+    if channel_desc is not None:
+        return channel_desc.group(1)
+    else:
+        return None
+
+def _get_request(url):
+    """
+    Returns HTML of given URL
+    
+    """
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0'
     }
-    req = requests.get('https://youtube.com/' + channel_name + '/about', headers=headers)
-    channel_desc = re.search(r'description" content="(.*?)"', req.text)
-    return channel_desc[0]
+    req = requests.get(url, headers=headers)
+    return req.text
 
 def _strip_non_numeric(item):
     """
