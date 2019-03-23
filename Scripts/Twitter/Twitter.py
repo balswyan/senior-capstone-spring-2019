c76bbb829ca959dd25ba1a52498bfc7229d3f859
rsajjan3 <32497743+rsajjan3@users.noreply.github.com>
2019-03-23 17:54:48 -0400

    Updated Twitter parser

--- Scripts/Twitter/Twitter.py
+++ Scripts/Twitter/Twitter.py
@@ -1,33 +1,44 @@
-import re
-from bs4 import BeautifulSoup
-
-def get_url_info(html_soup):
-
-    title = html_soup.find_all('title')
-    if len(title) > 0:
-        title_clean = _clean_html(str(title[0]))
-    else:
-        title_clean = ''
-    
-    bio = html_soup.select_one(".ProfileHeaderCard-bio").get_text()
-    tweet = html_soup.select_one(".js-tweet-text-container .TweetTextSize--jumbo").get_text()
-    html_info = {
-        'title': title_clean,
-        'description': bio,
-        'paragraphs': tweet
-        #'images': images
-    }
-    return html_info
+import tweepy
+import pickle
+import json
 
 
-def _clean_html(raw_html_string):
+def get_url_info(tweet_id, input_path):
+    try:
+        api = _connect_to_twitter_api(input_path, 'key.p')
+        tweet = api.get_status(tweet_id, tweet_mode='extended')
+        
+        tweet_text = tweet.full_text
+        user_bio = tweet.user.description
+
+        images = []
+        for media in tweet.extended_entities["media"]:
+            if media['type'] == 'photo':
+                    images.append(media['media_url'])
+        
+        tweet_info = {
+            "title": tweet_text,
+            "description": user_bio,
+            "paragraphs": "",
+            "images": images,
+        }
+        return tweet_info
+    except tweepy.TweepError as error:
+        return error.reason
+
+def _connect_to_twitter_api(keys_path, key_name):
     """
-    To remove html tags from strings.
+    This function connets to the Twitter API via tweepy and returns an API
+    object.
     
-    :param raw_html_string (string): the string with html tags.
-    
-    :return clean_html_string (string): the string without html tags.
+    'keys_path' = (string)  Path where your key is located
+    'key_name' = (string)   Name of the key object. Pickle file containing a
+                            dictionary with the following keys: 'ckey', 
+                            'csecret', 'atoken', and 'asecret'.
     """
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', raw_html_string).decode('utf-8', 'ignore').encode('ascii', 'ignore') # Helps with encoding issues on RT and WashPost
-    return cleantext
+    # - import pickle file with the key
+    key = pickle.load(open('%s%s' % (keys_path, key_name), 'rb'))
+    auth = tweepy.OAuthHandler(key['ckey'], key['csecret'])
+    auth.set_access_token(key['atoken'], key['asecret'])
+    api = tweepy.API(auth)
+    return(api)
