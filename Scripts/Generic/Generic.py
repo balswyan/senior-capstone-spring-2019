b6b81271c6dc0c3a0df3b82a9e064fd192f5ffc7
rsajjan3 <32497743+rsajjan3@users.noreply.github.com>
2019-03-23 15:19:17 -0400

    Updated generic path

--- Scripts/Generic/Generic.py
+++ Scripts/Generic/Generic.py
@@ -8,7 +8,7 @@ import json
 
 def main():
     print(os.getcwd())
-    output_path = 'Scripts/NYTimes/'
+    output_path = 'Scripts/Generic/'
     html_info = get_url_info('Page.html', output_path)
     json.dump(html_info, open('%s/%s.json' % (output_path, "Page"), 'w'))
 
